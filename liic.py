dbd3a8b4ef2826eff41c9e9c71b8b80c727ed72d
joecooper98 <joe@ak-srv1.ed.ac.uk>
2021-09-07 14:47:35 +0100

    fixes

--- liic.py
+++ liic.py
@@ -28,6 +28,9 @@ for line in end_file:
 
 pathwayname = input("What do you want the pathway files to be called?\n") # the name of the intermediate files
 
+no_atoms = end_geom.index('Variables:') - 5 
+print(no_atoms)
+
 this_is_crap_but_it_works = [] # retrieve all variable names by splitting the lines. The bits after the line with "Variables:" are the variable names
 for i in range(len(start_geom)):
     split=start_geom[i].split(" ")
@@ -48,9 +51,10 @@ for i in range(5,end_geom.index('Variables:')): # warning if molecules have diff
 geoms = [] # init blank list for all the variables. will be an array of arrays for variables at each intermediate pathway point.
 for i in range(no_of_points):
     geoms.append([]) # add a blank list to the list
-    for j in range(end_geom.index('Variables:')+1,len(end_geom)): # only iterate over the variable section of the file
+    for j in range(end_geom.index('Variables:')+1,end_geom.index('Variables:')+1+3*no_atoms-6): # only iterate over the variable section of the file
+        print(end_geom[j].split()[1])
         difference=float(end_geom[j].split()[-1])-float(start_geom[j].split()[-1]) # find the difference between the start and end points variables
-        increment = difference / no_of_points # create the increment, which is simply the difference divided by the number of points
+        increment = difference / (no_of_points+1) # create the increment, which is simply the difference divided by the number of points
         ithgeom = []
         ithgeom.append(float(start_geom[j].split()[-1])+increment*(i+1)) # add the increment onto the variables, and add the variables to the blank list at its index inside the list of geometries
         geoms[i].append(ithgeom)
@@ -63,12 +67,13 @@ for j in range(no_of_points):
         if i <= start_geom.index("Variables:"): # copy start geom file up to the word Variables
             f.write(start_geom[i])
             f.write("\n")
-        elif i < len(start_geom)-1: # write the variable name, then a space, then the variable (without []), and then a new line.
+        elif i < len(start_geom): # write the variable name, then a space, then the variable (without []), and then a new line.
             f.write(this_is_crap_but_it_works[i]+" "+str(geoms[j][i-start_geom.index("Variables:")-1]).strip("[]")+"\n")
+#            print(i)
         else:
             f.write("\n") # end with new line
     f.close() #close file
     os.system("obabel -igzmat "+name+" -oxyz > "+name+".xyz") # convert files back into .xyz files
 
 os.system("obabel -igzmat "+startfile+" -oxyz > "+pathwayname+"_0.xyz") # make start file an .xyz
-    
+os.system("obabel -igzmat "+startfile+" -oxyz > "+pathwayname+"_"+str(no_of_points+1)+".xyz") # make start file an .xyz
